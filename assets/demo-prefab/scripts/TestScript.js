f3bf3cfbca7bb8986338f2ecf20efb5bee424e83
bedau221 <quy.nguyen@enotion.io>
2022-12-22 17:48:42 +0700

    create scene with some type of assets

---
+++ assets/demo-prefab/scripts/TestScript.js
@@ -0,0 +1,12 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        holder: cc.Node,
+        label: cc.Label,
+        spine: sp.Skeleton,
+        sprite: cc.Sprite,
+        audio: cc.AudioSource
+    },
+});
+
