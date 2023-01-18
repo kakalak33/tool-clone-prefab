efe47d8f66eaf9320e39b55c44dc0c812196d57d
kakalak33 <tu.tran@enotion.io>
2023-01-18 15:41:24 +0700

    - update localize tool

--- packages/localize-tool/main.js
+++ packages/localize-tool/main.js
@@ -1,51 +1,61 @@
 'use strict';
-
-
 function readScene(path) {
-  const { readFileSync } = require('fs');
-  const sceneString = readFileSync(path, { encoding: 'UTF-8' })
-  return JSON.parse(sceneString);
+	const { readFileSync } = require('fs');
+	const sceneString = readFileSync(path, { encoding: 'UTF-8' })
+	return JSON.parse(sceneString);
+}
+
+function removeComponent(sceneUuid, targetUuid) {
+	const scenePath = Editor.assetdb.uuidToFspath(sceneUuid);
+	let sceneArr = readScene(scenePath);
+	const { writeFileSync } = require('fs')
+
+	// get all nodes having this script
+	const scriptedNodes = [];
+	sceneArr.forEach(sceneInfo => {
+		if (sceneInfo.__type__ == targetUuid) {
+			scriptedNodes.push(sceneArr[sceneInfo.node.__id__]);
+		}
+	})
+
+	// if node have sprite comp => set spriteFrame null
+	scriptedNodes.forEach(nodeInfo => {
+		if (nodeInfo) {
+			const { _components } = nodeInfo;
+			if (_components) {
+				_components.forEach(({ __id__ }) => {
+					if (sceneArr[__id__].__type__ == 'cc.Sprite') {
+						sceneArr[__id__]._spriteFrame = null;
+					}
+				})
+			}
+		}
+	})
+
+	// overwrite scene file and refresh db
+	writeFileSync(scenePath, JSON.stringify(sceneArr))
 }
 
 module.exports = {
 
-  messages: {
-    'open'() {
-      // open entry panel registered in package.json
-      Editor.Panel.open('localize-tool');
-    },
-    'clicked'(evt, scenePath, compressedUuid) {
-      Editor.log('Tool run')
-      let sceneArr = readScene(scenePath);
-      let assetdbRootPath = 'db://assets';
-      const { writeFileSync } = require('fs')
+	messages: {
+		'log'() {
+			
+		},
+		'open'() {
+			// open entry panel registered in package.json
+			Editor.Panel.open('localize-tool');
+		},
+		'clicked'(evt, folderUuid, targetUuid) {
+			const targetFolder = Editor.assetdb.uuidToUrl(folderUuid);
 
-      // get all nodes having this script
-      const scriptedNodes = [];
-      sceneArr.forEach(sceneInfo => {
-        if (sceneInfo.__type__ == compressedUuid) {
-          scriptedNodes.push(sceneArr[sceneInfo.node.__id__]);
-        }
-      })
-
-      // if node have sprite comp => set spriteFrame null
-      scriptedNodes.forEach(nodeInfo => {
-        if (nodeInfo) {
-          const { _components } = nodeInfo;
-          if (_components) {
-            _components.forEach(({ __id__ }) => {
-              if (sceneArr[__id__].__type__ == 'cc.Sprite') {
-                sceneArr[__id__]._spriteFrame = null;
-              }
-            })
-          }
-        }
-      })
-
-      // overwrite scene file and refresh db
-      writeFileSync(scenePath, JSON.stringify(sceneArr))
-      Editor.assetdb.refresh(assetdbRootPath, () => Editor.log('Refresh database'));
-      Editor.log('Localization succeeds');
-    },
-  },
+			Editor.assetdb.queryAssets(`${targetFolder}/**\/*`, ['scene', 'prefab'], (err, results) => {
+				results.forEach(({ uuid, url }) => {
+					Editor.log('=== Remove component in ' + url + ' ===');
+					removeComponent(uuid, targetUuid);
+				})
+			})
+			Editor.assetdb.refresh(targetFolder, () => Editor.log('=== Refresh folder ==='));
+		},
+	},
 };
--- packages/localize-tool/panel/index.js
+++ packages/localize-tool/panel/index.js
@@ -4,37 +4,39 @@ Editor.Panel.extend({
   style: `
     :host { margin: 5px; }
     h2 { color: #f90; }
-    .dragBox { width:200px; position: relative; left: 50px }
+    .dragBox { width:200px; position: relative; left: 125px }
   `,
 
   // html template for panel
   template: `
     <h2>localize-tool</h2>
     <hr />
-    <div>Scene</div>
-    <ui-asset value="" class="dragBox"id="scene"></ui-asset>
+    <div>Folder</div>
+    <ui-asset id="folderAsset" class="dragBox" type="folder" droppable="asset" tabindex="-1" empty=""></ui-asset>
     <hr />
-    <div>Script</div>
+    <div>Target Component</div>
     <ui-asset value="" class="dragBox"id="script"></ui-asset>
-    <hr />
-    <ui-button id="btn">Localize</ui-button>
+    <hr /> 
+    <ui-button id="btnStart">Remove Component</ui-button>
+    <ui-button id="btnLog">Log</ui-button>
   `,
 
   // element and variable binding
   $: {
-    btn: '#btn',
-    scene: '#scene',
+    btnStart: '#btnStart',
+    btnLog: '#btnLog',
+    folderAsset: '#folderAsset',
     script: '#script'
   },
 
   // method executed when template and styles are successfully loaded and initialized
   ready() {
-    this.$btn.addEventListener('confirm', () => {
-      const uuid = this.$scene.value;
+    this.$btnStart.addEventListener('confirm', () => {
       const compressedUuid = Editor.Utils.UuidUtils.compressUuid(this.$script.value);
-      Editor.assetdb.queryPathByUuid(uuid, (err, results) => {
-        Editor.Ipc.sendToMain('localize-tool:clicked', results, compressedUuid);
-      });
+      Editor.Ipc.sendToMain('localize-tool:clicked', this.$folderAsset.value, compressedUuid);
+    });
+    this.$btnLog.addEventListener('confirm', () => {
+
     });
   },
 
